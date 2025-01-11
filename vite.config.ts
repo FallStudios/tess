6aa408ab62292433dc44b3708a954183982bbd6b
TEZZ <teztezishere@gmail.com>
2025-01-11 09:57:17 +0300

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/', // Changed to root path since we're using a custom domain
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+});
